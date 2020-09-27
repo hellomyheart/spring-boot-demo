0a4767bdd96c681ca82d885b1b8916d0dcac574d
stephen shen <723809534@qq.com>
2020-09-27 23:31:25 +0800

    Spring Boot 整合MyBatis准备工作

---
+++ spring-boot-test3/src/main/java/cn/hellomyheart/springboottest2/filter/LoginFilter.java
@@ -0,0 +1,30 @@
+package cn.hellomyheart.springboottest2.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @description 登录过滤器
+ * @className: LoginFilter
+ * @package: cn.hellomyheart.springboottest2.filter
+ * @author: Stephen Shen
+ * @date: 2020/9/27 下午9:32
+ */
+//指定拦截路径
+@WebFilter(urlPatterns = "/filter/**")
+//还需要这个注解，创建SpringBean对象
+@Component
+public class LoginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("LoginFilter");
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        filterChain.doFilter(request,response);
+    }
+}
